5177cfaa196a37518a49ff06a393d8f25d91e328
Juanda999 <juanda999games@gmail.com>
2026-03-25 00:47:34 -0500

    Add sample message sender manager

---
+++ Assets/Scripts/Manager/MessageSenderManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Firestore;
+public class MessageSenderManager : MonoBehaviour
+{
+    private FirebaseFirestore db;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        db = FirebaseFirestore.DefaultInstance;
+    }
+    [ContextMenu("Send Message")]
+    public void SendMessage()
+    {
+        Dictionary<string,object> message = new Dictionary<string, object>()
+        {
+            {"text","Try jumped lololol"},
+            {"posX", 10f},
+            {"posY", 20f},
+            {"posZ", 50f},
+        };
+
+        db.Collection("messages").AddAsync(message);
+    }
+}
